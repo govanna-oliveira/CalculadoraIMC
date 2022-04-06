b32a0da76edf0b86376995c1b039931870c0c612
Geovanna Oliveira <geovannaoliveira169@gmail.com>
2022-04-06 11:57:41 -0300

    Calculo IMC

--- Program.cs
+++ Program.cs
@@ -17,8 +17,55 @@ namespace CalculadoraIMC
 
             double imc = peso / (altura * altura);
 
-            Console.WriteLine("{0} sua altura é {1} e seu peso {2}", nome, altura, peso);
+            Console.WriteLine("{0} sua altura é: {1} e seu peso é: {2}", nome, altura, peso);
             Console.WriteLine("Seu IMC é {0}", imc);
-        }   
+
+            if (imc < 17.00)
+            {
+                Console.WriteLine("Muito abaixo do peso!");
+            }
+
+            else if (imc >= 17.00 && imc < 18.50)
+            {
+                Console.WriteLine("Abaixo do peso!");
+            }
+             else if (imc >= 18.50 && imc < 25.00)
+            {
+                Console.WriteLine("Peso normal!");
+            } 
+            else if (imc >= 25.00 && imc < 30.00)
+            {
+                Console.WriteLine("Sobrepeso!");
+            } 
+            else if (imc >= 30.00 && imc < 35.00)
+            {
+                Console.WriteLine("Obesidade grau I!");
+            }
+            else if (imc >= 35.00 && imc < 40.00)
+            {
+                Console.WriteLine("Obesidade grau II!");
+            }
+            else if (imc >= 40.00 && imc < 45.00)
+            {
+                Console.WriteLine("Obesidade grau III ou mórbida!");
+            }
+             
+             if (imc < 18.50)
+             {
+                 double pesoIdeal = 18.50;
+                 double pesoNecessario = Math.Round(pesoIdeal - imc, 2);
+
+                 Console.WriteLine("Você precisa ganhar {0} Kg!", pesoNecessario);
+             }
+             else if(imc >= 25.00)
+             {
+                 double pesoIdeal = 18.50;
+                 double pesoNecessario = Math.Round(pesoIdeal - imc, 2);
+
+                 Console.WriteLine("Você precisa perder {0} Kg!", pesoNecessario);
+             }
+
+        }  
+            
     }
 }
