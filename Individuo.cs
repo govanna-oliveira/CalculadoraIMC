dc049add7f6c0a050e1e7e50dfe069c56914158e
geovanna-oliveira <102254889+govanna-oliveira@users.noreply.github.com>
2022-04-24 18:19:57 -0300

    Update Individuo.cs

--- Individuo.cs
+++ Individuo.cs
@@ -1,3 +1,16 @@
-public class Individuo {
-    
+public class Individuo 
+{
+  public float Altura {get; set;} 
+  public float Peso {get; set;}
+
+  public Individuo(float altura, float peso) 
+  { 
+     Altura = altura;
+     Peso = peso; 
+  }
+
+  public double IMC { get => Peso / (Altura * altura);}
+
 }
+
+    
